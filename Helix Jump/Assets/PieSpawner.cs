c247784b34fde47aec1e440d0f4ba80005c67756
jc <jiowchern@gmail.com>
2020-01-05 17:53:59 +0800

    first upload

---
+++ Helix Jump/Assets/PieSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieSpawner : MonoBehaviour
+{
+    public Transform Root;
+    public GameObject Origin        ;
+    public int PieCount;
+    public float Space;
+    void Start()
+    {
+        var originPosition = Origin.transform.position;
+        for (var i = 1; i <= PieCount; ++i)
+        {
+            var pitObj = GameObject.Instantiate(Origin);
+            pitObj.transform.position = new Vector3(originPosition.x , originPosition.y - i * Space, originPosition.z);
+            pitObj.SetActive(true);
+            pitObj.transform.SetParent(Root);
+        }
+    }
+
+    
+    
+}
---
+++ Helix Jump/Assets/Project/Scrips/Reflect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reflect : MonoBehaviour
+{
+    public Vector3 Force;
+    readonly static Regulus.Utility.TimeCounter _CollisionCooldown = new Regulus.Utility.TimeCounter();
+    readonly static long _CooldownTicks = (long)(((double)Regulus.Utility.TimeCounter.SecondTicks) * 0.2);
+    public Reflect()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (_CollisionCooldown.Ticks < _CooldownTicks)
+            return;
+
+        var rig = collision.gameObject.GetComponent<Rigidbody>();
+        rig.velocity = Vector3.zero;
+        rig.AddForce(Force, ForceMode.Impulse);
+
+        _CollisionCooldown.Reset();
+    }
+}
